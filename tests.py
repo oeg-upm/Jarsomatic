fd0c99824e08208e580c85a38862e937765d3119
Ahmad Alobaid <ahmad88me@gmail.com>
2016-06-09 17:24:47 +0200

    added empty tests

---
+++ tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+
+class IntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        # clone the testing repo
+        # clean Vocabularies.csv in readme and master
+        # push the changes
+        # get the run
+        # wait until it is completed
+
+    def tearDown(self):
+        pass
+
+    def test1(self):
+        pass
+
+    def test2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
